ca5cf69acb015a4e876da9d15881539a8801d60f
Crowdbotics <engineering@crowdbotics.com>
2022-10-18 00:25:35 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Addresswooblhzprw(models.Model):
     "Generated Model"
     street = models.BigIntegerField()
+    ca = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
--- backend/home/views.py
+++ backend/home/views.py
@@ -1,3 +1,5 @@
+from .models import Addresswooblhzprw
+from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.shortcuts import render
 
 
